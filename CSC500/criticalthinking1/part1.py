fc725d8765bf9b81b233c274264905fe9a024e32
GreatWhiteGing <bhergen@comcast.net>
2024-05-13 14:40:00 -0600

    Add files via upload

---
+++ CSC500/criticalthinking1/part1.py
@@ -0,0 +1,7 @@
+# Get numbers from input
+num1 = int(input("Please enter a number: "))
+num2 = int(input("Please enter another number: "))
+
+# Output the addition and subtraction of the numbers
+print("Addition: ", (num1 + num2))
+print("Subtraction: ", (num1 - num2))
---
+++ CSC500/criticalthinking1/part2.py
@@ -0,0 +1,7 @@
+# Get numbers from input
+num1 = int(input("Please enter a number: "))
+num2 = int(input("Please enter another number: "))
+
+# Output the multiplication and division of the numbers
+print("Multiplied: ", (num1 * num2))
+print("Divided: ", (num1 / num2))
